0e062d90590fc9d2f7a80285105b587d41ba8cf2
Caleb Chiang <calebchiang1002@gmail.com>
2025-03-13 10:58:00 -0700

    Updated styling for sign up page

--- client/src/app/auth/signup/page.js
+++ client/src/app/auth/signup/page.js
@@ -1,11 +1,21 @@
 import "../../styles/signup-page.css";
+import Link from "next/link";
 
 export default function SignupPage() {
-    return (
-      <div className="signup-container">
-        <h1>Sign Up Page</h1>
-        <p>Sign-up functionality will be implemented later.</p>
-      </div>
-    );
-  }
+  return (
+    <div className="signup-container">
+      <h1 className="signup-title">Sign Up</h1>
+      <form className="signup-form">
+        <input type="text" placeholder="Display Name" className="signup-input" />
+        <input type="email" placeholder="Email" className="signup-input" />
+        <input type="password" placeholder="Password" className="signup-input" />
+        <button type="submit" className="signup-button">Sign Up</button>
+      </form>
+
+      <p className="signup-login-text">
+        Already have an account? <Link href="/auth/login" className="signup-login-link">Login here</Link>
+      </p>
+    </div>
+  );
+}
   
