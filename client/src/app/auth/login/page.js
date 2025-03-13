a8334b132f4f0e791df5be5d0b208e22713e22b9
Caleb Chiang <calebchiang1002@gmail.com>
2025-03-13 11:09:42 -0700

    Updated styling for login page

--- client/src/app/auth/login/page.js
+++ client/src/app/auth/login/page.js
@@ -1,11 +1,19 @@
 import "../../styles/login-page.css";
+import Link from "next/link";
 
 export default function LoginPage() {
-    return (
-      <div className="login-container">
-        <h1>Login Page</h1>
-        <p>Login functionality will be implemented later.</p>
-      </div>
-    );
-  }
-  
+  return (
+    <div className="login-container">
+      <h1 className="login-title">Login</h1>
+      <form className="login-form">
+        <input type="email" placeholder="Email" className="login-input" />
+        <input type="password" placeholder="Password" className="login-input" />
+        <button type="submit" className="login-button">Login</button>
+      </form>
+
+      <p className="login-signup-text">
+        Don't have an account? <Link href="/auth/signup" className="login-signup-link">Sign up here</Link>
+      </p>
+    </div>
+  );
+}
