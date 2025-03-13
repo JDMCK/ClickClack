1141bb0562e91ff1e3e146428825538afc844c13
Caleb Chiang <calebchiang1002@gmail.com>
2025-03-13 09:05:51 -0700

    Added login and signup routes inside auth directory

---
+++ client/src/app/auth/login/page.js
@@ -0,0 +1,9 @@
+export default function LoginPage() {
+    return (
+      <div>
+        <h1>Login Page</h1>
+        <p>Login functionality will be implemented later.</p>
+      </div>
+    );
+  }
+  
---
+++ client/src/app/auth/signup/page.js
@@ -0,0 +1,9 @@
+export default function SignupPage() {
+    return (
+      <div>
+        <h1>Sign Up Page</h1>
+        <p>Sign-up functionality will be implemented later.</p>
+      </div>
+    );
+  }
+  
