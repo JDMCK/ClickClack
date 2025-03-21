26df1399b6febb6491257a6b73b03ed197ec8cbc
Caleb Chiang <calebchiang1002@gmail.com>
2025-03-21 10:30:58 -0700

    Added functions to return accuracy, wpm, and awpm

--- server/utils/store_test.js
+++ server/utils/store_test.js
@@ -1,6 +1,5 @@
 // Helper functions for storing tests
 
-// Returns accuracy
 export const getAccuracy = (keyStrokes, prompt) => {
     const promptKeys = prompt.split('');
     
@@ -21,3 +20,38 @@ export const getAccuracy = (keyStrokes, prompt) => {
 
     return Math.floor((correct / (correct + error)) * 100) || 0; // Returns percentage
 };
+
+export const getWPM = (keyStrokes, duration) => {
+  const filteredKeystrokes = keyStrokes.filter(k => k !== 'Backspace');
+
+  const wordsTyped = filteredKeystrokes.length / 5;
+  const minutes = duration / 60;
+
+  return minutes > 0 ? Math.floor(wordsTyped / minutes) : 0;
+}
+
+export const getAWPM = (keyStrokes, duration, prompt) => {
+  const wpm = getWPM(keyStrokes, duration);
+  const minutes = duration / 60;
+
+  const finalTyped = [];
+  for (const key of keyStrokes) {
+    if (key === "Backspace") {
+      finalTyped.pop(); 
+    } else {
+      finalTyped.push(key); 
+    }
+  }
+
+  let uncorrectedErrors = 0;
+  for (let i = 0; i < Math.max(finalTyped.length, prompt.length); i++) {
+    if (finalTyped[i] !== prompt[i]) {
+      uncorrectedErrors++;
+    }
+  }
+
+  const awpm = Math.max(Math.floor(wpm - (uncorrectedErrors / minutes)), 0);
+  return awpm;
+};
+
+
