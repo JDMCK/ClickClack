aab7765e29ccea3e238e0d1e45f6cd35be314ffb
Abdulqadir_Abuharrus <aabuharrus@gmail.com>
2025-03-27 15:47:50 -0700

    Update not-found styles

--- client/src/app/not-found.js
+++ client/src/app/not-found.js
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react';
 import Link from "next/link";
 
@@ -6,9 +7,9 @@ export default function NotFound() {
     <div className="not-found">
       <div className='container'>
         <h1>Quack! 404</h1>
-        <p>Page not found or doesn't exist...</p>
-        <Link className='' href="/">Go back</Link>
-        <img src="/path-to-your-image.jpg" alt="404 image" />
+        <img className='not-found-img' src="/duck2.png" alt="404 image" />
+        <p style={{fontSize: '1.2rem'}}>Page not found or doesn't exist...</p>
+        <button className='go-back' onClick={() => {window.location = '/home'}}>Go back</button>
       </div>
     </div>
   );
