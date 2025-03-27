106679fd8a6582cd18b967f223d6ddcf3a365c5c
Abdulqadir_Abuharrus <aabuharrus@gmail.com>
2025-03-27 16:10:43 -0700

    Replace `<img>` tag with Next.js `<Image>`

--- client/src/app/not-found.js
+++ client/src/app/not-found.js
@@ -1,13 +1,14 @@
 "use client"
 import React from 'react';
 import Link from "next/link";
+import Image from "next/image";
 
 export default function NotFound() {
   return (
     <div className="not-found">
       <div className='container'>
         <h1>Quack! 404</h1>
-        <img className='not-found-img' src="/duck2.png" alt="404 image" />
+        <Image className='not-found-img' src="/duck2.png" alt="404 image" width={500} height={500} />
         <p style={{fontSize: '1.2rem'}}>Page not found or doesn't exist...</p>
         <button className='go-back' onClick={() => {window.location = '/home'}}>Go back</button>
       </div>
