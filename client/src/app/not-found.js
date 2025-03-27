c3b07ed5a4d47ea0f52cb233b553d050df2ca53d
Abdulqadir_Abuharrus <aabuharrus@gmail.com>
2025-03-26 21:17:27 -0700

    Reorganize and refactor 404 files

--- client/src/app/not-found.js
+++ client/src/app/not-found.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import './404.css'; // Assuming you have a CSS file for styling
-
+import Link from "next/link";
 
 export default function NotFound() {
   return (
     <div className="not-found">
       <h1>Quack! 404</h1>
       <p>Page not found or doesn't exist...</p>
-      <Link className='' to="/">Go back</Link>
+      <Link className='' href="/">Go back</Link>
     </div>
   );
 };
