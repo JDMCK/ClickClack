57d0eb7c6d3ab0b56e687a6bfa977cb11d524b8e
Abdulqadir_Abuharrus <aabuharrus@gmail.com>
2025-03-31 22:47:02 -0700

    Implement boost user tokens

--- client/src/app/profile/admin/page.js
+++ client/src/app/profile/admin/page.js
@@ -7,20 +7,20 @@ import { getUserProfile, SERVER_BASE_URL } from '../../../utils/api'
 import { logOutReq } from '../../../utils/api';
 
 export default function UserProfile() {
-  const router = useRouter()
-  const [error, setError] = useState(null);
-  const [displayName, setDisplayName] = useState(null)
-  const [apiTokens, setApiTokens] = useState(0)
-  const [usersData, setUsersData] = useState([]);
-  const [loading, setLoading] = useState(false);
+    const router = useRouter()
+    const [error, setError] = useState(null);
+    const [displayName, setDisplayName] = useState(null)
+    const [apiTokens, setApiTokens] = useState(0)
+    const [usersData, setUsersData] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     /**
      * Fetch the Users data for the Admin dash board
      */
-    const fetchUsersData = async () =>{
+    const fetchUsersData = async () => {
         setLoading(true);
         setError(null);
-        try{
+        try {
             const response = await fetch(`${SERVER_BASE_URL}/users/admin/`, {
                 method: "GET",
                 headers: {
@@ -29,49 +29,75 @@ export default function UserProfile() {
                 credentials: 'include'
             });
 
-            if(!response.ok){
+            if (!response.ok) {
                 throw new Error("Failed to fectch users' data for admin page!");
             }
             const res = await response.json();
             setUsersData(res.data)
-        }catch(error){
+        } catch (error) {
             console.log("Error fetching data: ", error);
             setError(error);
-        }finally{
+        } finally {
             setLoading(false);
         }
     }
 
+    const boostUserTokens = async (userId) => {
+        setLoading(true)
+        setError(null)
+        console.log("userid: ", userId)
+        try {
+            const response = await fetch(`${SERVER_BASE_URL}/users/boost-tokens`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                credentials: 'include',
+                body: JSON.stringify({ userId })
+            });
+            if (!response.ok) {
+                throw new Error("Failed to fectch users' data for admin page!");
+            }
+            const res = await response.json();
+            console.log("User boosted: ", res)
+        } catch (error) {
+            console.log("Error boosting: ", error)
+            setError(error)
+        } finally {
+            setLoading(false)
+        }
+    }
 
-    useEffect(() =>{
-    const fetchProfile = async () => {
+
+    useEffect(() => {
+        const fetchProfile = async () => {
+            setLoading(true)
+            setError(null)
+            try {
+                const response = await getUserProfile();
+                setApiTokens(response.tokenCount);
+                setDisplayName(response.displayName);
+
+            } catch (error) {
+                console.log("Error fetching profile", error);
+                setError(error)
+            } finally {
+                setLoading(false)
+            }
+        }
+        fetchProfile();
+        fetchUsersData();
+    }, [])
+
+    const logout = async () => {
         setLoading(true)
         setError(null)
         try {
-        const response = await getUserProfile();
-        setApiTokens(response.tokenCount);
-        setDisplayName(response.displayName);
-    
-        } catch (error) {
-        console.log("Error fetching profile", error);
-        setError(error)
-        } finally {
-        setLoading(false)
-        }
-    }
-    fetchProfile();
-    fetchUsersData();
-    }, [])
-
-    const logout = async () =>{
-        setLoading(true)
-        setError(null)
-        try{
             const response = await logOutReq();
             console.log(response);
-        }catch(error){
+        } catch (error) {
             setError(error)
-        }finally{
+        } finally {
             setLoading(false)
             router.push('/')
         }
@@ -79,62 +105,67 @@ export default function UserProfile() {
 
     return (
         <>
-        <div className="container">
-            <h1><em>You&apos;re an Admin Duck, {displayName}!</em></h1>
+            <div className="container">
+                <h1><em>You&apos;re an Admin Duck, {displayName}!</em></h1>
 
-            <h2>All Ducklings (users)</h2>
-            <div className='content-box'>
-            <table className="user-table">
-                <thead>
-                    <tr>
-                        <th>Username</th>
-                        <th>Tokens Available</th>
-                        <th>Role</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {loading ? (
-                        <tr>
-                            <td colSpan="3">Loading...</td>
-                        </tr>
-                    ) : error ? (
-                        <tr>
-                            <td colSpan="3">Error loading data</td>
-                        </tr>
-                    ) : (
-                        usersData && usersData.length > 0 ? (
-                            usersData.map((user, index) => (
-                                <tr key={index}>
-                                    <td>{user.displayName}</td>
-                                    <td>{user.tokenCount}</td>
-                                    <td>{user.role}</td>
-                                </tr>
-                            ))
-                        ) : (
+                <h2>All Ducklings (users)</h2>
+                <div className='content-box'>
+                    <table className="user-table">
+                        <thead>
                             <tr>
-                                <td colSpan="3">No data available</td>
+                                <th>Username</th>
+                                <th>Tokens Available</th>
+                                <th>Role</th>
+                                <th>Actions</th>
                             </tr>
-                        )
-                    )}
-                </tbody>
-            </table>
-        </div>
-            <div className='content-box'>
-                <div className='content' >
-                    <h4>Start another run?</h4>
-                    <p>Generate new text or use a previous one.</p>
-                    <button onClick={() => { router.push('/typing/prompt'); }}>Start</button>
+                        </thead>
+                        <tbody>
+                            {loading ? (
+                                <tr>
+                                    <td colSpan="4">Loading...</td>
+                                </tr>
+                            ) : error ? (
+                                <tr>
+                                    <td colSpan="4">Error loading data</td>
+                                </tr>
+                            ) : (
+                                usersData && usersData.length > 0 ? (
+                                    usersData.map((user, index) => (
+                                        <tr key={index}>
+                                            <td>{user.displayName}</td>
+                                            <td>{user.tokenCount}</td>
+                                            <td>{user.role}</td>
+                                            <td>
+                                                <button onClick={() => boostUserTokens(user.userid)}>
+                                                    Boost Tokens
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    ))
+                                ) : (
+                                    <tr>
+                                        <td colSpan="4">No data available</td>
+                                    </tr>
+                                )
+                            )}
+                        </tbody>
+                    </table>
                 </div>
-                <div className='content'>
-                    <h4>Control Pannel</h4>
-                    <p>Remaining tokens: {apiTokens}</p>
-                    <button type="submit" className="logout-btn" disabled={loading} onClick={() =>
-                         {logout()}}>{loading ? "Logging out..." : "Logout"}</button>
+                <div className='content-box'>
+                    <div className='content' >
+                        <h4>Start another run?</h4>
+                        <p>Generate new text or use a previous one.</p>
+                        <button onClick={() => { router.push('/typing/prompt'); }}>Start</button>
+                    </div>
+                    <div className='content'>
+                        <h4>Control Pannel</h4>
+                        <p>Remaining tokens: {apiTokens}</p>
+                        <button type="submit" className="logout-btn" disabled={loading} onClick={() => { logout() }}>{loading ? "Logging out..." : "Logout"}</button>
 
+                    </div>
                 </div>
+                <Scoreboard />
             </div>
-            <Scoreboard />
-        </div>
         </>
     )
 }
